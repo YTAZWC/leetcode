d9020f1c8826f8a30a010a4da14292b8543b0d82
YTAZWC <18570354653@163.com>
2023-11-26 16:19:12 +0800

    34.在排序数组中查找元素的第一个和最后一个位置

--- src/leetcode/editor/cn/T_34_FindFirstAndLastPositionOfElementInSortedArray.java
+++ src/leetcode/editor/cn/T_34_FindFirstAndLastPositionOfElementInSortedArray.java
@@ -12,52 +12,102 @@ public class T_34_FindFirstAndLastPositionOfElementInSortedArray {
 	 public static void main(String[] args) {
 	     //测试代码
 	     Solution solution = new T_34_FindFirstAndLastPositionOfElementInSortedArray().new Solution();
-		 System.out.println(Arrays.toString(solution.searchRange(new int[]{5, 7, 7, 8, 8, 10}, 8)));
+		 System.out.println(Arrays.toString(solution.searchRange(new int[]{5, 7, 7, 8, 8, 10}, 6)));
 	 }
 	 
 // 力扣代码
 //leetcode submit region begin(Prohibit modification and deletion)
 class Solution {
     public int[] searchRange(int[] nums, int target) {
-		int start = -1;	// 存储元素的第一个位置
-		int end = -1;	// 存储元素的最后一个位置
+/*		// 获取 target 的右边界且不包含target
+		int rightBorder = getRightBorder(nums, target);
+		// 获取 target 的左边界且不包含target
+		int leftBorder = getLeftBorder(nums, target);
 
-		// 左闭右开区间 查找元素的最后一个位置
+		if (rightBorder == -2 || leftBorder == -2) {
+			// 说明target在数组范围的左边或右边
+			return new int[]{-1, -1};
+		}
+		rightBorder -= 1;
+		leftBorder += 1;
+		if (nums[rightBorder] != target || nums[leftBorder] != target) {
+			// 说明target在数组范围内 但不在数组中
+			return new int[]{-1, -1};
+		}
+		return new int[]{leftBorder, rightBorder};*/
+
+		// 解法二
+		// 二分查找 数组中是否存在目标值
+		int flag = searchBinary(nums, target);
+		if (flag == -1) {
+			return new int[]{-1, -1};
+		}
+		// 如果存在 则滑动指针来寻找
+		int right = flag;		// 根据查找到的位置来寻找右边界
+		while (right + 1 < nums.length && nums[right + 1] == target)
+			++ right;
+		int left = flag;		// 根据查找到的位置来寻找左边界
+		while (left - 1 >= 0 && nums[left - 1] == target)
+			-- left;
+		return new int[]{left, right};
+    }
+
+	private int searchBinary(int[] nums, int target) {
 		int left = 0;
-		int right = nums.length;
-		while (left < right) {
+		int right = nums.length - 1;
+		while (left <= right) {
+			int mid = ((right - left) >> 1) + left;
+			if (nums[mid] > target) {
+				right = mid - 1;
+			} else if (nums[mid] < target) {
+				left = mid + 1;
+			} else {
+				return mid;
+			}
+		}
+		return -1;
+	}
+
+	// 二分法寻找target的左边界 且不包含target
+	// 使用第一种二分 左闭右闭 [left, right]
+	// 若leftBorder没有被赋值 说明target在数组范围的右边
+	/*private int getLeftBorder(int[] nums, int target) {
+		int leftBorder = -2;	// 记录左边界的值 且检验是否被赋值
+		int left = 0;
+		int right = nums.length -1;
+		while (left <= right) {        // left == right 有意义
 			int mid = ((right - left) >> 1) + left;		// 防止溢出
 			if (nums[mid] < target) {
-				// 说明目标值在区间的右半部分 [mid+1, right)
 				left = mid + 1;
-			} else if (nums[mid] > target) {
-				// 说明目标值在区间的左半部分 [left, mid)
-				right = mid;
 			} else {
-				// 查找到元素的最后一个位置 退出循环
-				end = mid;
-				break;
-			}
-		}
-
-		// 左开右闭区间 查找元素的第一个位置
-		left = -1;
-		right = nums.length-1;
-		while (left < right) {
-			int mid = ((right - left) >> 1) + left;
-			if (nums[mid] < target) {	// 说明目标值在区间右半部分 (mid, right]
-				left = mid;
-			} else if (nums[mid] > target) { 	// 说明目标值在区间左半部分 (left, mid-1]
 				right = mid - 1;
-			} else {	// 查找到元素的第一个位置 退出循环
-				start = mid;
-				break;
+				leftBorder = right;
 			}
 		}
+		return leftBorder;
+	}
 
-
-		return new int[] {start, end};
-    }
+	// 二分查找寻找target的右边界 且不包含target
+	// 使用第一种二分 左闭右闭[left, right]
+	// 若rightBorder没有被赋值 说明target在数组范围的左边
+	private int getRightBorder(int[] nums, int target) {
+		int rightBorder = -2;		// 记录rightBorder是否被赋值 即是否有搜索到右边界
+		int left = 0;
+		int right = nums.length - 1;
+		while (left <= right) {		// 左闭右闭 left==right 有意义
+			int mid = ((right - left) >> 1) + left;		// 防止溢出
+			if (nums[mid] > target) {
+				// nums[mid] > target 说明目标值在 左区间[left, mid-1]
+				right = mid - 1;
+			} else {
+				// nums[mid] <= target 说明右边界在右区间
+				// 当 nums[mid] == target 时 对右边界进行更新 得到不包含target的右边界
+				left = mid + 1;
+				rightBorder = left;
+			}
+		}
+		return rightBorder;
+	}*/
 }
 //leetcode submit region end(Prohibit modification and deletion)
 
