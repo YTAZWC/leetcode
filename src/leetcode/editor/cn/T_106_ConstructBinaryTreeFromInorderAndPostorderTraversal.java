9112ea894658205bb65c8f2e26c4bb356b76850c
hoshino <2802470158@qq.com>
2024-01-27 20:47:53 +0800

    106.从中序与后序遍历序列构造二叉树

---
+++ src/leetcode/editor/cn/T_106_ConstructBinaryTreeFromInorderAndPostorderTraversal.java
@@ -0,0 +1,42 @@
+package leetcode.editor.cn;
+
+
+import utils.TreeNode;
+
+/**
+ * 106.从中序与后序遍历序列构造二叉树
+ * @author 花木凋零成兰
+ * @date 2024-01-22 14:06:22
+ */
+public class T_106_ConstructBinaryTreeFromInorderAndPostorderTraversal {
+	 public static void main(String[] args) {
+	     //测试代码
+	     Solution solution = new T_106_ConstructBinaryTreeFromInorderAndPostorderTraversal().new Solution();
+	 
+	 }
+	 
+// 力扣代码
+//leetcode submit region begin(Prohibit modification and deletion)
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public TreeNode buildTree(int[] inorder, int[] postorder) {
+
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
+
+}
