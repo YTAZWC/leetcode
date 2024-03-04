82b75b25b48a2cd6c0313e10f737a1982f489b33
YTAZWC <18570354653@163.com>
2024-03-04 16:35:27 +0800

    21.合并两个有序链表   未解决

---
+++ src/leetcode/editor/cn/T_21_MergeTwoSortedLists.java
@@ -0,0 +1,37 @@
+package leetcode.editor.cn;
+
+
+import utils.ListNode;
+
+/**
+ * 21.合并两个有序链表
+ * @author 花木凋零成兰
+ * @date 2024-03-04 16:34:26
+ */
+public class T_21_MergeTwoSortedLists {
+	 public static void main(String[] args) {
+	     //测试代码
+	     Solution solution = new T_21_MergeTwoSortedLists().new Solution();
+	 
+	 }
+	 
+// 力扣代码
+//leetcode submit region begin(Prohibit modification and deletion)
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
+
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
+
+}
