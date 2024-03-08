7e87bfb9c3e2010c3eba749a0520d082070c0861
YTAZWC <18570354653@163.com>
2024-03-08 16:31:41 +0800

    491.非递减子序列  回溯+哈希+优化

--- src/leetcode/editor/cn/T_491_NonDecreasingSubsequences.java
+++ src/leetcode/editor/cn/T_491_NonDecreasingSubsequences.java
@@ -18,33 +18,53 @@ public class T_491_NonDecreasingSubsequences {
 // 力扣代码
 //leetcode submit region begin(Prohibit modification and deletion)
 class Solution {
+//	List<List<Integer>>	ans = new ArrayList<>();
+//	List<Integer> path = new ArrayList<>();
+//	Set<List<Integer>> hash = new HashSet<>();
+//    public List<List<Integer>> findSubsequences(int[] nums) {
+//		backtracking(0, nums);	// 回溯函数
+//		return ans;
+//    }
+//	private void backtracking(int startIndex, int[] nums) {
+//		if (path.size() >= 2) {
+//			// System.out.println(path);
+//			if (!hash.contains(path)) {
+//				ArrayList<Integer> newPath = new ArrayList<>(path);
+//				ans.add(newPath);
+//				hash.add(newPath);
+//			}
+//		}
+//		for (int i = startIndex; i < nums.length; ++ i) {
+//			if (path.isEmpty()) {	// 初始时 子序列为空
+//				path.add(nums[i]);	// 直接添加元素
+//			} else {
+//				if (i != startIndex && nums[i] == nums[i-1])
+//					continue;	// 树层去重
+//				int pre = path.get(path.size()-1);
+//				if (nums[i] < pre) continue;	// 去除非递增元素
+//				path.add(nums[i]);	// 添加符合条件的数组元素
+//			}
+//			backtracking(i+1, nums);		// 继续递归遍历
+//			path.remove(path.size() - 1);	// 回溯
+//		}
+//	}
 	List<List<Integer>>	ans = new ArrayList<>();
 	List<Integer> path = new ArrayList<>();
-	Set<List<Integer>> hash = new HashSet<>();
-    public List<List<Integer>> findSubsequences(int[] nums) {
-		backtracking(0, nums);	// 回溯函数
+	public List<List<Integer>> findSubsequences(int[] nums) {
+		backtracking(0, -101, nums);	// 回溯函数
 		return ans;
-    }
-	private void backtracking(int startIndex, int[] nums) {
+	}
+	private void backtracking(int startIndex, int last, int[] nums) {
 		if (path.size() >= 2) {
-			// System.out.println(path);
-			if (!hash.contains(path)) {
-				ArrayList<Integer> newPath = new ArrayList<>(path);
-				ans.add(newPath);
-				hash.add(newPath);
-			}
+			ans.add(new ArrayList<>(path));
 		}
+		boolean[] hash = new boolean[201];
 		for (int i = startIndex; i < nums.length; ++ i) {
-			if (path.isEmpty()) {	// 初始时 子序列为空
-				path.add(nums[i]);	// 直接添加元素
-			} else {
-				if (i != startIndex && nums[i] == nums[i-1])
-					continue;	// 树层去重
-				int pre = path.get(path.size()-1);
-				if (nums[i] < pre) continue;	// 去除非递增元素
-				path.add(nums[i]);	// 添加符合条件的数组元素
-			}
-			backtracking(i+1, nums);		// 继续递归遍历
+			if (hash[nums[i]+100]) continue;	// 树层去重
+			if (!path.isEmpty() && nums[i] < last) continue; // 排除不符合题意的元素
+			hash[nums[i]+100] = true;
+			path.add(nums[i]);
+			backtracking(i+1, nums[i], nums);		// 继续递归遍历
 			path.remove(path.size() - 1);	// 回溯
 		}
 	}
