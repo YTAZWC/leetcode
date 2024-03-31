72435ff203617cb80ca4d8278b63d4ad8be54869
YTAZWC <18570354653@163.com>
2024-03-31 14:16:58 +0800

    509.斐波那契数  状态压缩

--- src/leetcode/editor/cn/T_509_FibonacciNumber.java
+++ src/leetcode/editor/cn/T_509_FibonacciNumber.java
@@ -16,17 +16,32 @@ public class T_509_FibonacciNumber {
 // 力扣代码
 //leetcode submit region begin(Prohibit modification and deletion)
 class Solution {
-    public int fib(int n) {
+//    public int fib(int n) {
+//		if (n == 0 || n == 1)
+//			return n;
+//		int[] dp = new int[n+1];
+//		dp[0] = 0;
+//		dp[1] = 1;
+//		for (int i = 2; i <= n; ++ i) {
+//			dp[i] = dp[i-1] + dp[i-2];
+//		}
+//		return dp[n];
+//    }
+
+	public int fib(int n) {
 		if (n == 0 || n == 1)
 			return n;
-		int[] dp = new int[n+1];
-		dp[0] = 0;
-		dp[1] = 1;
+		int ans = 0;
+		int a = 0, b = 1;	// 用于表示推导下一层状态的两个状态
 		for (int i = 2; i <= n; ++ i) {
-			dp[i] = dp[i-1] + dp[i-2];
+			ans = a + b;	// 状态转移
+			// 更新两个状态
+			a = b;
+			b = ans;
 		}
-		return dp[n];
-    }
+		return ans;
+	}
+
 }
 //leetcode submit region end(Prohibit modification and deletion)
 
