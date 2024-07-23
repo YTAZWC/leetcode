36e68f0cfcc765778a507f36b9f785eaecad99bf
花木凋零成兰 <18570354653@163.com>
2024-07-23 17:30:12 +0800

    SQL

---
+++ src/leetcode/editor/cn/T_175_CombineTwoTables.java
@@ -0,0 +1,18 @@
+package leetcode.editor.cn;
+
+
+/**
+ * 175.组合两个表
+ * @author 花木凋零成兰
+ * @date 2024-07-23 17:29:31
+ */
+public class T_175_CombineTwoTables {
+	 public static void main(String[] args) {
+	     //测试代码
+	     Solution solution = new T_175_CombineTwoTables().new Solution();
+	 
+	 }
+	 
+// 力扣代码
+//There is no code of Java type for this problem
+}
