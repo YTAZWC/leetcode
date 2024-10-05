0e74d81eed378f01671734b8b0225f2742ef7e2b
花木凋零成兰 <18570354653@163.com>
2024-10-05 11:27:35 +0800

    206.反转链表

--- src/leetcode/editor/cn/T_206_ReverseLinkedList.java
+++ src/leetcode/editor/cn/T_206_ReverseLinkedList.java
@@ -2,7 +2,7 @@ package leetcode.editor.cn;
 
 
 /**
- * 反转链表
+ * 206.反转链表
  * @author 花木凋零成兰
  * @date 2023-12-06 21:38:20
  */
@@ -27,19 +27,21 @@ public class T_206_ReverseLinkedList {
  */
 class Solution {
 //    public ListNode reverseList(ListNode head) {
-//		if (head == null)	// head为空 直接返回
+//		if (head == null) {
 //			return null;
-//		// p为q的前一个节点
-//		ListNode p = head;
-//		ListNode q = head.next;
-//		while (q != null) {
-//			ListNode t = q.next;	// 记录下q的下一个节点
-//			q.next = p;		// 改变节点的指向 将其反转指向前一个节点
-//			p = q;			// 重新移动p为 未反转节点的前一个节点
-//			q = t;			// 移动q到还未反转的节点
 //		}
-//		head.next = null;	// 第一个节点的指向需要改为空
-//		return p;		// 返回新的头节点 即原链表的尾节点
+//		ListNode p = null;
+//		ListNode q = head;
+//		while (q != null) {
+//			// 记录下一个反转的节点
+//			ListNode temp = q.next;
+//			// 改变节点指向 反转指向前一个节点
+//			q.next = p;
+//			// 重新移动p为 新链表头节点
+//			p = q;
+//			q = temp;	// 移动q继续反转
+//		}
+//		return p;
 //    }
 	// 递归
 	public ListNode reverseList(ListNode head) {
