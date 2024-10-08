6881f899a72695fcdc58c379f16b9211adb83414
花木凋零成兰 <18570354653@163.com>
2024-10-08 17:27:44 +0800

    56.合并区间

--- src/leetcode/editor/cn/T_56_MergeIntervals.java
+++ src/leetcode/editor/cn/T_56_MergeIntervals.java
@@ -22,14 +22,11 @@ public class T_56_MergeIntervals {
 //leetcode submit region begin(Prohibit modification and deletion)
 class Solution {
     public int[][] merge(int[][] intervals) {
-		Arrays.sort(intervals, new Comparator<int[]>() {
-			@Override
-			public int compare(int[] interval1, int[] interval2) {
-				if (interval1[0] == interval2[0])	// 若左边界相等则按右边界升序排
-					return interval1[1] - interval2[1];
-				else return interval1[0] - interval2[0];	// 对区间按照 左边界升序排列
-			}
-		});
+		Arrays.sort(intervals, (interval1, interval2) -> {
+            if (interval1[0] == interval2[0])	// 若左边界相等则按右边界升序排
+                return interval1[1] - interval2[1];
+            else return interval1[0] - interval2[0];	// 对区间按照 左边界升序排列
+        });
 		List<int[]> ans = new ArrayList<>();	// 存储合并后的区间
 		int left = intervals[0][0];		// 记录合并后的区间左边界
 		int right = intervals[0][1];	// 记录合并后的区间右边界
@@ -43,8 +40,9 @@ class Solution {
 				left = intervals[i][0];
 				right = intervals[i][1];
 			}
-			if (i == len-1)		// 将最后一个区间记录到合并区间集合中
-				ans.add(new int[]{left, right});
+			if (i == len-1)	{	// 将最后一个区间记录到合并区间集合中
+                ans.add(new int[]{left, right});
+            }
 		}
 		return ans.toArray(new int[ans.size()][]);	// 合并区间后 返回结果
     }
